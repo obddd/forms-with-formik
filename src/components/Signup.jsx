e19202aac64a228aa3eac383ea585898a167fdeb
obddd <obaidulhaq20@gmail.com>
2020-12-28 22:54:53 +0500

    implemented formik

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -8,14 +8,14 @@ import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 
 import * as yup from 'yup';
+import { Formik, Form } from 'formik';
 
 let SignUpSchema = yup.object().shape({
-  firstname: yup.string().required('FirstName is required!'),
-  lastname: yup.string().required('LastName is required!'),
-  email: yup.email().required('Email is required!'),
+  firstname: yup.string().required('Firstname is required!'),
+  lastname: yup.string().required('Lastname is required!'),
+  email: yup.string().email().required('Email is required!'),
   password: yup
     .string()
-    .min(6, 'Password is too short.')
     .max(20, 'Password is too long.')
     .required('This field is required.'),
 });
@@ -48,69 +48,110 @@ const useStyles = makeStyles((theme) => ({
 const Signup = () => {
   const classes = useStyles();
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <form className={classes.form}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="outlined"
+    <Formik
+      initialValues={{
+        firstname: '',
+        lastname: '',
+        email: '',
+        password: '',
+      }}
+      validationSchema={SignUpSchema}
+      onSubmit={(values) => console.log(values)}
+    >
+      {({ errors, handleChange, touched }) => (
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <div className={classes.paper}>
+            <Typography component="h1" variant="h5">
+              Sign up
+            </Typography>
+            <Form className={classes.form}>
+              <Grid container spacing={2}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    error={errors.firstname && touched.firstname}
+                    autoComplete="fname"
+                    name="firstname"
+                    variant="outlined"
+                    fullWidth
+                    onChange={handleChange}
+                    id="firstname"
+                    label="First Name"
+                    autoFocus
+                    helperText={
+                      errors.firstname && touched.firstname
+                        ? errors.firstname
+                        : null
+                    }
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    error={errors.lastname && touched.lastname}
+                    autocomplete="lname"
+                    variant="outlined"
+                    fullWidth
+                    onChange={handleChange}
+                    id="lastname"
+                    label="Last Name"
+                    name="lastname"
+                    autoFocus
+                    helperText={
+                      errors.lastname && touched.lastname
+                        ? errors.lastname
+                        : null
+                    }
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    error={errors.email && touched.email}
+                    autocomplete="email"
+                    variant="outlined"
+                    fullWidth
+                    onChange={handleChange}
+                    id="email"
+                    label="Email"
+                    name="email"
+                    autoFocus
+                    helperText={
+                      errors.email && touched.email ? errors.email : null
+                    }
+                  />
+                </Grid>
+                <Grid item xs={12}>
+                  <TextField
+                    error={errors.password && touched.password}
+                    autocomplete="current-password"
+                    variant="outlined"
+                    fullWidth
+                    onChange={handleChange}
+                    id="password"
+                    label="Password"
+                    name="password"
+                    autoFocus
+                    helperText={
+                      errors.password && touched.password
+                        ? errors.password
+                        : null
+                    }
+                  />
+                </Grid>
+              </Grid>
+              <Button
+                type="submit"
                 fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="outlined"
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="outlined"
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign Up
-          </Button>
-        </form>
-      </div>
-    </Container>
+                variant="contained"
+                color="primary"
+                className={classes.submit}
+              >
+                Sign Up
+              </Button>
+            </Form>
+          </div>
+        </Container>
+      )}
+    </Formik>
   );
 };
 
